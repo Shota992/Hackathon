2ca987526ec9b89b970d389b4ef6db886e612b2a
shota0827 <posse22sho@gmail.com>
2024-11-15 20:56:30 +0900

    新規投稿画面のviewファイル作成した

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//新規投稿登録画面のルート設定
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+//新規投稿登録を保存するルート設定
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+
 
 require __DIR__.'/auth.php';
