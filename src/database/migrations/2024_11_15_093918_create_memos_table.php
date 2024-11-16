90e6bd450ad40784e025adfd03962abba01fb95c
yuzuki0127 <funamoto112987@gmail.com>
2024-11-17 07:15:14 +0900

    メモ編集

--- src/database/migrations/2024_11_15_093918_create_memos_table.php
+++ src/database/migrations/2024_11_15_093918_create_memos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('content');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('posting_id');
+            $table->unsignedBigInteger('posting_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
             
