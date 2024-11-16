7a3c529f9b88277d2fb70f188cfbdc103196df95
shota0827 <posse22sho@gmail.com>
2024-11-17 03:46:45 +0900

    メモ一覧のフロントエンドにサイドバーコンポーネントを追加

--- src/app/Http/Controllers/MemoController.php
+++ src/app/Http/Controllers/MemoController.php
@@ -4,20 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Memo;
+use Illuminate\Support\Facades\Auth;
+
 
 
 
 class MemoController extends Controller
 {
-    public function index(){
-        $memos = Memo::all();
-        return view('memo.index', ['memos' => $memos]);
+    public function index()
+    {
+        $user = Auth::user(); // ログイン中のユーザーを取得
+        $memos = Memo::all(); // すべてのメモを取得
+        return view('memo.index', compact('user', 'memos')); // 両方のデータをビューに渡す
     }
-    
+
     public function memocreate()
     {
         return view('memo/create');
     }
+
 
     // public function store(Request $request)
     // {
@@ -37,4 +42,3 @@ class MemoController extends Controller
     //     return redirect()->route('timeline.index')->with('success', 'メモが作成されました！');
     // }>>>>>>> ac235eed54afc19643930fc0813440eb4ba3f62a
 }
-
