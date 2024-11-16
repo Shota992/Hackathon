970be7300892b61d544bc5587acdb2ff5fb5019e
yuzuki0127 <funamoto112987@gmail.com>
2024-11-17 07:03:01 +0900

    見た目、メモ修正

--- src/resources/views/components/body-common.blade.php
+++ src/resources/views/components/body-common.blade.php
@@ -1,14 +1,20 @@
 <x-app-layout>
-  <main class="flex">
-      <x-sidebar :user="Auth::user()" />
+    <main class="flex">
+        <x-sidebar :user="Auth::user()" />
 
-      <div class="main w-full bg-white h-screen ml-60 pt-10 px-12 pb-20">
-          <h2 class="text-2xl mb-4 border-b-2 font-adamina">{{ $title }}</h2>
-          <div class="px-8">
-              <div class="w-full h-full bg-gray-200 p-10 overflow-y-auto">
-                  {{ $slot }}
-              </div>
-          </div>
-      </div>
-  </main>
+        <div class="main w-full bg-white h-screen ml-60 pt-10 px-12 pb-20">
+            <h2 class="text-2xl mb-4 border-b-2 font-adamina">{{ $title }}</h2>
+            <div class="px-8">
+                @if (session('success'))
+                    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4"
+                        role="alert">
+                        <span class="block sm:inline">{{ session('success') }}</span>
+                    </div>
+                @endif
+                <div class="w-full h-full bg-gray-200 p-10 overflow-y-auto">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+    </main>
 </x-app-layout>
